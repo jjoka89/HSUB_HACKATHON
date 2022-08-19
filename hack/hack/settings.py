54d888342fbb7756d1652eeb815308783723c443
kimyeonho <dksk87721@gmail.com>
2022-08-20 01:40:39 +0900

    hsu

--- hack/hack/settings.py
+++ hack/hack/settings.py
@@ -121,6 +121,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.Account'
 
 
-
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
 CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
